c563c2765e7bf5a3df5a4672700d05485020c436
vidhipatel24 <36531514+vidhipatel24@users.noreply.github.com>
2018-11-21 22:22:17 +0530

    Making dynamic table & filling color in cell

--- designs.js
+++ designs.js
@@ -1,10 +1,37 @@
 // Select color input
+var height,width,color;
 // Select size input
-
+var table = $('#pixelCanvas');
 // When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
+$(document).ready(function (){
+  $('#sizePicker').submit(function (event){
+    event.preventDefault();
+    var height = $('#inputHeight').val();
+    var weight = $('#inputWeight').val();
+    makeGrid(height,weight);
+   })
+function makeGrid(N,M) {
+  $('tr').remove();
+  //to make user defined grid
+  for(let i=1;i<=N;i++){
+    table.append('<tr id=row' + i '></tr>');
+      for(let j=1;j<=M;j++){
+        $('#row'+i).append('<td></td>');
+      }
+  }
+  //To add color to the grid
+  $('td').click(function addcolor(){
+    color = $('#colorPicker').val();
+    
+    if($(this).attr('style')){
+      $(this).removeAttr('style')
+    }
+    else
+    {
+      $(this).attr('style','background-color:' + color);
+    }
+  })
+}
 
 // Your code goes here!
-
-}
+});
